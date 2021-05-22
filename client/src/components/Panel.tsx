3c6482a88b1c1766d160fe70fe1a19ae39542d3d
Aksh Bansal <akshmark@gmail.com>
2021-05-23 01:15:16 +0530

    feat(client): add color picker

--- client/src/components/Panel.tsx
+++ client/src/components/Panel.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { panelStyles } from "../styles/panel";
 import ClearButton from "./controllingStuff/ClearButton";
+import ColorPicker from "./controllingStuff/ColorPicker";
 
 const Panel: React.FC = () => {
   return (
     <div style={panelStyles}>
       <ClearButton />
+      <ColorPicker />
     </div>
   );
 };
---
+++ client/src/components/controllingStuff/ColorPicker.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useCanvasStore } from "../../store/canvasStore";
+
+const ColorPicker: React.FC = () => {
+  const [color, setColor] = useState("black");
+  const ctx = useCanvasStore((state) => state.reff);
+  const handleColor = (newColor: string) => {
+    setColor(newColor);
+    ctx.current.strokeStyle = color;
+  };
+  return (
+    <input
+      value={color}
+      onChange={(e) => handleColor(e.target.value)}
+      type="color"
+      name="color"
+      id="colorpicker"
+    />
+  );
+};
+
+export default ColorPicker;
