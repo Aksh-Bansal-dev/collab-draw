b18bdb46974ca86f7343e64c07805efdb21c3e33
Aksh Bansal <akshmark@gmail.com>
2021-05-23 18:55:31 +0530

    feat: show eraser cursor when used

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Collab-Draw
 
 Draw on the same canvas at the same time using web sockets.
+
+## Features
+
+- change color
+- change thickness of brush
+- clear screen
+- eraser
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,17 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Canvas from "../components/Canvas";
 import Panel from "../components/Panel";
+import { useCanvasStore } from "../store/canvasStore";
+import styles from "../styles/homepage.module.css";
 
 const Homepage: React.FC = () => {
+  const color = useCanvasStore((state) => state.color);
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
+    <div className={color === "grey" ? styles.eraser : styles.normal}>
       <Canvas />
       <Panel />
     </div>
